a9adc8600c748119947f2aecbac3f58405a7a310
zz-wa <204535176+zz-wa@users.noreply.github.com>
2026-04-21 16:56:14 +0800

    refactor(server): extrac command handlers and unify response

--- server/server.go
+++ server/server.go
@@ -24,91 +24,86 @@ func HandleConn(conn net.Conn) {
 		}
 		input = strings.TrimSpace(input)
 		in := strings.Split(input, " ")
-		if in[0] == "set" {
-			var ttl int64
-
-			if len(in) != 3 && len(in) != 4 {
-				conn.Write([]byte("wrong input" + "\n"))
-				continue
-			}
-			if len(in) == 4 {
-				ttl, _ = strconv.ParseInt(in[3], 10, 64)
-			}
-			if txn.IsActive() {
-				txn.RecordFirst(in[1])
-			}
-
-			store.Set(in[1], in[2], ttl)
-
-			conn.Write([]byte("ok\n"))
-		}
-		if in[0] == "get" {
-			if len(in) < 2 {
-				conn.Write([]byte("wrong input" + "\n"))
-				continue
-			}
-			value, ok := store.Get(in[1])
-			if !ok {
-				conn.Write([]byte("key not found\n"))
-			} else {
-				conn.Write([]byte(value + "\n"))
-			}
-
-		}
-		if in[0] == "del" {
-
-			if len(in) < 2 {
-				_, err := conn.Write([]byte("wrong input\n"))
-				if err != nil {
-					return
-				}
-				continue
-			}
-			if txn.IsActive() {
-				txn.RecordFirst(in[1])
-			}
-
-			err := store.Del(in[1])
-			if err != nil {
-				_, err2 := conn.Write([]byte("err\n"))
-				if err2 != nil {
-					return
-				}
-				continue
-			}
-			_, err = conn.Write([]byte("ok\n"))
-			if err != nil {
-				return
-			}
-		}
-		if in[0] == "begin" {
-			err := txn.Begin()
-			if err != nil {
-				conn.Write([]byte(err.Error() + "\n"))
-				continue
-			}
-			conn.Write([]byte("ok\n"))
-			continue
-		}
-		if in[0] == "commit" {
-			err := txn.Commit()
-			if err != nil {
-				conn.Write([]byte(err.Error() + "\n"))
-				continue
-			}
-			conn.Write([]byte("ok\n"))
-			continue
-		}
-		if in[0] == "rollback" {
-			err := txn.Rollback()
-			if err != nil {
-				conn.Write([]byte(err.Error() + "\n"))
-				continue
-			}
-			conn.Write([]byte("ok\n"))
-
-			continue
+		switch in[0] {
+		case "set":
+			handleSet(conn, in, txn)
+		case "get":
+			handleGet(conn, in)
+		case "del":
+			handleDel(conn, in, txn)
+		case "begin":
+			handleBegin(conn, txn)
+		case "commit":
+			handleCommit(conn, txn)
+		case "rollback":
+			handleRollback(conn, txn)
+		default:
+			conn.Write([]byte("unknown command\n"))
 		}
 	}
+}
+func handleSet(conn net.Conn, in []string, txn *Txn) {
+	var ttl int64
 
+	if len(in) != 3 && len(in) != 4 {
+		conn.Write([]byte("wrong input\n"))
+		return
+	}
+	if len(in) == 4 {
+		ttl, _ = strconv.ParseInt(in[3], 10, 64)
+	}
+	if txn.IsActive() {
+		txn.RecordFirst(in[1])
+	}
+
+	err := store.Set(in[1], in[2], ttl)
+	reply(conn, err)
+}
+func handleGet(conn net.Conn, in []string) {
+	if len(in) < 2 {
+		conn.Write([]byte("wrong input\n"))
+		return
+	}
+	value, ok := store.Get(in[1])
+	if !ok {
+		conn.Write([]byte("key not found\n"))
+	} else {
+		conn.Write([]byte(value + "\n"))
+	}
+
+}
+func handleDel(conn net.Conn, in []string, txn *Txn) {
+	if len(in) < 2 {
+		conn.Write([]byte("wrong input\n"))
+		return
+	}
+	if txn.IsActive() {
+		txn.RecordFirst(in[1])
+	}
+
+	err := store.Del(in[1])
+	reply(conn, err)
+
+}
+
+func handleBegin(conn net.Conn, txn *Txn) {
+	err := txn.Begin()
+	reply(conn, err)
+}
+func handleCommit(conn net.Conn, txn *Txn) {
+	err := txn.Commit()
+	reply(conn, err)
+
+}
+func handleRollback(conn net.Conn, txn *Txn) {
+	err := txn.Rollback()
+	reply(conn, err)
+
+}
+func reply(conn net.Conn, err error) {
+	if err != nil {
+		conn.Write([]byte(err.Error() + "\n"))
+		return
+	}
+	conn.Write([]byte("ok\n"))
 }
